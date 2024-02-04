ac0a3dbd736ca64e6c2899578d531f98b5044e03
Santhossh15 <santhossh15@gmail.com>
2024-02-04 11:49:25 +0530

    Added TopBar Component

---
+++ blog_platform/src/App.js
@@ -0,0 +1,14 @@
+import Topbar from "./NavBar/Topbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <p>
+        tes
+      </p>
+    </div>
+  );
+}
+
+export default App;
