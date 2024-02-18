7112502811b4139297c33a4b6e52464ee5dfe8c6
Santhossh15 <santhossh15@gmail.com>
2024-02-18 12:42:50 +0530

    Final Commit

---
+++ personal_portfolio_website/src/utils/data.js
@@ -0,0 +1,63 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "./assets/images/frontend-icon.png",
+        skills: [
+            { skill: "HTML5", percentage: "90%" },
+            { skill: "CSS3", percentage: "80%" },
+            { skill: "JavaScript", percentage: "90%" },
+            { skill: "React JS", percentage: "80%" },
+            { skill: "Angular JS", percentage: "70%" },
+            { skill: "Redux Toolkit", percentage: "85%" },
+        ],
+    },
+    {
+        title: "Backend",
+        icon: "./assets/images/backend-icon.png",
+        skills: [
+            { skill: "Node.js", percentage: "80%" },
+            { skill: "Express.js", percentage: "75%" },
+            { skill: "MongoDB", percentage: "80%" }
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "./assets/images/tools-icon.png",
+        skills: [
+            { skill: "Git & GitHub", percentage: "85%" },
+            { skill: "Visual Studio Code", percentage: "75%" },
+            { skill: "Webpack", percentage: "60%" },
+            { skill: "Responsive Design", percentage: "70%" },
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "./assets/images/soft-skills-icon.png",
+        skills: [
+            { skill: "Self-Learning", percentage: "90%" },
+            { skill: "Collaboration", percentage: "85%" },
+            { skill: "Teaching", percentage: "90%" },
+        ],
+    },
+];
+
+export const WORK_EXPERIENCE = [
+    {
+        title: "MERN Stack Intern at BETTER TOMORROW",
+        date: "June 2023 - July 2023",
+        responsibilities: [
+            "Independently crafted MERN stack applications, showcasing expertise in MongoDB, Express.js, React.js, and Node.js.",
+            "Implemented robust functionalities, elevating skills in full-stack development and contributing to individual project success.",
+            "Conducted code reviews and mentored junior developers in adopting best coding practices.",
+        ],
+    },
+    {
+        title: "REACT JS Developer Intern at CODSOFT",
+        date: "December 2023 - January 2024",
+        responsibilities: [
+            "Developed three React projects (Task Tracker, Portfolio, Blog) demonstrating strong problemsolving and collaboration skills.",
+            "Implemented advanced CSS animations and transitions to enhance the overall user experience.",
+            "Proficient in React.js, state management, and project delivery.",
+        ],
+    },
+];
